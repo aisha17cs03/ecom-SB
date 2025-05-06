4125ebff59a6edf66fd54386f82f01d9d80913fc
Aisha Tayyaba <aisha17cs03@gmail.com>
2025-05-07 02:16:20 +0530

    creating a table in h2database

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,6 +1,16 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+//this particular class is an entity and it will create a table in db with class name so in db table name is - category
+@Entity
+//or
+//if we need give sab specific table name so if db table name is-categories
+//@Entity(name="catgories")
 public class Category {
+    //categoryId to be our unique identifier anf in db it is marked as primary key
+    @Id
     //setting the attributes of category
     private Long categoryId;
     private String categoryName;
