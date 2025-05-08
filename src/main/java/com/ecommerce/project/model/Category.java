176465606d8d3b0d1fb3b9721b34fd8430a06c7b
Aisha Tayyaba <aisha17cs03@gmail.com>
2025-05-09 02:26:36 +0530

    adding a Genration

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 //this particular class is an entity and it will create a table in db with class name so in db table name is - category
@@ -9,8 +11,11 @@ import jakarta.persistence.Id;
 //if we need give sab specific table name so if db table name is-categories
 //@Entity(name="catgories")
 public class Category {
-    //categoryId to be our unique identifier anf in db it is marked as primary key
+    //categoryId to be our unique identifier and in db it is marked as primary key
     @Id
+    //Identity strategy used in the db to genrate primary key values and this supported by relational db's like-postgresql, mysql
+    //Note- identity strategy is not supported by all the db's
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     //setting the attributes of category
     private Long categoryId;
     private String categoryName;
@@ -19,6 +24,12 @@ public class Category {
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+    }
+
+
+    //default constructor- write for good practice not mandatory to write
+    public Category(){
+
     }
 
     //getter
