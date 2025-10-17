e7b60cee87a07f251e35506c599d37024e66361f
Aisha Tayyaba <aisha17cs03@gmail.com>
2025-10-17 22:00:01 +0530

    Adding ProductDTO and ProductResponse

---
+++ src/main/java/com/ecommerce/project/Payload/ProductDTO.java
@@ -0,0 +1,21 @@
+package com.ecommerce.project.Payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//@Data annotation generates getters, setters, toString, equals, and hashCode methods
+//@NoArgsConstructor generates a no-argument constructor
+//@AllArgsConstructor generates a constructor with 1 parameter for each field in your class
+public class ProductDTO {
+    private Long productId;
+    private String productName;
+    private String image;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+}
